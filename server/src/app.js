a9013cdba8160ba25daad5cf0a55764a80ee9f90
Prashant <prashant@example.com>
2024-08-20 00:27:35 -0400

    add database setup

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+// Middleware to handle CORS and allow cross-origin requests from specified origins
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+//middleware to make data available with limit of 16kb to prevent DDOS
+app.use(express.json({limit: "16kb"}))
+// Middleware to parse incoming URL-encoded data from forms with a limit of 16kb
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+app.use(express.static("public"))
+
+export { app };
---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import { app } from "./app.js";
+import "dotenv/config";
+import connectDB from "./db/index.js";
+
+const PORT = process.env.PORT || 4000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
